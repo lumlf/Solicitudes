79bd418a69bd9ec896d67f68020780a0518bef3a
lumlf <lmlugof@correo.udistrital.edu.co>
2023-01-25 00:36:19 -0500

    conexión

---
+++ ConexionBD/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace ConexionBaseDatos
+{
+    internal class Conexion
+    {
+        public SqlConnection getConexion()
+        {
+            try
+            {
+                string cadena = "";
+            }
+            catch
+            {
+
+            }
+        }
+
+
+    }
+}
